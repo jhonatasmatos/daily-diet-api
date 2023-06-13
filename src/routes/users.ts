e306509193eb6d1c9dc63dc7951cf163e18c86cf
jhonatasmatos <jhonatas.santos.matos@gmail.com>
2023-06-13 16:27:31 +0100

    feat: added creat user and get users

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,39 @@
 import { FastifyInstance } from 'fastify'
-// import { knex } from '../database'
+import { z } from 'zod'
+import { knex } from '../database'
+import { randomUUID } from 'crypto'
 
 export async function userRoutes(app: FastifyInstance) {
-  app.get('/users', async (request, reply) => {
-    // await knex('users').select('*')
+  app.post('/users', async (request, reply) => {
+    const createUserBodySchema = z.object({
+      username: z.string(),
+    })
 
-    return reply.status(200).send()
+    const { username } = createUserBodySchema.parse(request.body)
+
+    await knex('users').insert({
+      id: randomUUID(),
+      username,
+    })
+
+    return reply.status(201).send()
+  })
+
+  app.get('/users', async () => {
+    const users = await knex('users').select()
+
+    return { users }
+  })
+
+  app.get('/users/:id', async (request) => {
+    const getUserParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getUserParamsSchema.parse(request.params)
+
+    const user = await knex('users').where({ id }).select()
+
+    return { user }
   })
 }

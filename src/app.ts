8278533ecd39d56ff7c40d9a662464fef38da696
jhonatasmatos <jhonatas.santos.matos@gmail.com>
2023-06-14 14:42:18 +0100

    chore: added cookie and prefix on routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 
 import { mealRoutes } from './routes/meals'
 import { userRoutes } from './routes/users'
 
 export const app = fastify()
 
-app.register(mealRoutes)
-app.register(userRoutes)
+app.register(cookie)
+app.register(mealRoutes, { prefix: 'meals' })
+app.register(userRoutes, { prefix: 'users' })

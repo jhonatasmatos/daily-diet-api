cefe29604a6302d4196557a28284eb504a6ed920
jhonatasmatos <jhonatas.santos.matos@gmail.com>
2023-06-14 14:44:39 +0100

    refactor: removed unused request and added cookies in post

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,36 +4,43 @@ import { knex } from '../database'
 import { randomUUID } from 'crypto'
 
 export async function userRoutes(app: FastifyInstance) {
-  app.post('/users', async (request, reply) => {
+  app.post('/', async (request, reply) => {
     const createUserBodySchema = z.object({
       username: z.string(),
     })
 
     const { username } = createUserBodySchema.parse(request.body)
 
-    await knex('users').insert({
-      id: randomUUID(),
-      username,
-    })
+    let sessionId = request.cookies.sessionId
 
-    return reply.status(201).send()
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/meals',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
+    const result = await knex('users')
+      .insert({
+        id: randomUUID(),
+        username,
+        session_id: sessionId,
+      })
+      .returning('*')
+
+    const user = result[0]
+
+    return reply.status(201).send({
+      status: 'success',
+      data: user,
+    })
   })
 
-  app.get('/users', async () => {
+  app.get('/', async () => {
     const users = await knex('users').select()
 
     return { users }
-  })
-
-  app.get('/users/:id', async (request) => {
-    const getUserParamsSchema = z.object({
-      id: z.string().uuid(),
-    })
-
-    const { id } = getUserParamsSchema.parse(request.params)
-
-    const user = await knex('users').where({ id }).select()
-
-    return { user }
   })
 }

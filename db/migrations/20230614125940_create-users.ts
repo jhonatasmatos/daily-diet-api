af8cfc6b2c038689ca0959e6e3de726c819310ce
jhonatasmatos <jhonatas.santos.matos@gmail.com>
2023-06-14 14:43:11 +0100

    refactor: adjusted the migrations

--- db/migrations/20230614125940_create-users.ts
+++ db/migrations/20230614125940_create-users.ts
@@ -4,6 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('users', (table) => {
     table.uuid('id').primary()
     table.text('username').notNullable()
+    table.uuid('session_id').after('id').index()
   })
 }
 
